a0935cef8e0de11ab424081aa5f43c0fde3d3c1b
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-17 17:25:14 -0600

    Setting up Formik

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Formik } from 'formik'
 
 export default function App() {
+  /**
+   * This can either be synchronous or asynchronous. We won't focus on the onSubmit
+   * handler but in general it is most likely going to be asynchronous as you submit
+   * data to the server.
+   */
+  const handleSubmit = (values) => {}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Formik initialValues={{}} onSubmit={handleSubmit}>
+      {() => {
+          return (
+            <View style={styles.container}>
+              <Text>Open up App.js to start working on your app!</Text>
+              <StatusBar style="auto" />
+            </View>
+          )
+      }}
+    </Formik>
   );
 }
 
