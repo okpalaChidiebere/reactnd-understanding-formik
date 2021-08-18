d04957c0f70f0855f809172b1acbddc1ed6cd359
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-17 19:13:55 -0600

    CheckBox field

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { Formik } from 'formik'
+import CheckBox from "@react-native-community/checkbox"
 
 export default function App() {
   /**
@@ -9,16 +10,19 @@ export default function App() {
    * handler but in general it is most likely going to be asynchronous as you submit
    * data to the server.
    */
-  const handleSubmit = (values) => {}
+  const handleSubmit = (values) => {
+    console.log(values)
+  }
 
   return (
     <Formik 
       initialValues={{ 
         firstName: "",
+        acceptTerms: true,
       }} 
       onSubmit={handleSubmit}
     >
-      {({ values, handleChange }) => {
+      {({ values, setFieldValue, handleChange, handleSubmit }) => {
           return (
             <View style={styles.container}>
               <StatusBar style="auto" />
@@ -27,6 +31,24 @@ export default function App() {
                 style={styles.inputStyle}
                 onChangeText={handleChange('firstName')}
                 placeholder="FirstName"
+              />
+              <View style={styles.section}>
+                <Text style={styles.paragraph}>Custom colored checkbox</Text>
+                <CheckBox
+                  style={styles.checkbox}
+                  value={values?.acceptTerms}
+                  onValueChange={v => setFieldValue('acceptTerms', v)}
+                  tintColor="#4630EB"
+                  onTintColor="#4630EB"
+                  onFillColor="#4630EB"
+                  onCheckColor="#ffff"
+                  //color={values.acceptTerms ? '#4630EB' : undefined}
+                />
+              </View>
+              <Button
+                color="#4630EB"
+                title='Submit'
+                onPress={handleSubmit}
               />
             </View>
           )
@@ -48,5 +70,16 @@ const styles = StyleSheet.create({
     borderColor: '#4e4e4e',
     padding: 12,
     marginBottom: 5,
-  }
+  },
+  section: {
+    flexDirection: 'row',
+    justifyContent:"space-between",
+    alignItems: 'center',
+  },
+  paragraph: {
+    fontSize: 15,
+  },
+  checkbox: {
+    margin: 8,
+  },
 });
